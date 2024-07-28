a400fb285d30a110295c0e97329b29ab1d3d4a04
Ian Atha <ian@atha.io>
2024-07-28 16:16:08 +0300

    load device and persistence path from env vars

--- main.go
+++ main.go
@@ -1,16 +1,28 @@
 package main
 
-import "log"
-
-const (
-	CommandPortPath = "/dev/ttyUSB0"
-	NotifyPortPath  = "/dev/ttyUSB2"
+import (
+	"log"
+	"os"
 )
+
+var (
+	CommandPortPath    = getEnv("COMMAND_PORT_PATH", "/dev/ttyUSB0")
+	NotifyPortPath     = getEnv("NOTIFY_PORT_PATH", "/dev/ttyUSB2")
+	DataPersistencePath = getEnv("DATA_PERSISTENCE_PATH", "inbox.json")
+)
+
+func getEnv(key, defaultValue string) string {
+	value := os.Getenv(key)
+	if value == "" {
+		return defaultValue
+	}
+	return value
+}
 
 func main() {
 	AssertHuaweiModemMode()
 	log.Printf("Staring daemon at http://localhost:8080")
-	mon := NewMonitor(CommandPortPath, NotifyPortPath, "inbox.json")
+	mon := NewMonitor(CommandPortPath, NotifyPortPath, DataPersistencePath)
 
 	if err := mon.Run(); err != nil {
 		log.Fatalln(err)
